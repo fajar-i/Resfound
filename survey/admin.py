f6e479b3cb077a300219de94b6ea70fb311442ab
Bjari <fajariarkan@gmail.com>
2024-11-14 13:36:04 +0700

    test model

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Survey, Question, Response, SurveyResponse, QuestionOrder, RecommendedSurvey, QuestionType, ResponseChoice
 
-# Register your models here.
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(QuestionOrder)
+admin.site.register(QuestionType)
+admin.site.register(ResponseChoice)
+admin.site.register(SurveyResponse)
+admin.site.register(Response)
+admin.site.register(RecommendedSurvey)
--- survey/tests.py
+++ survey/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Survey, Question, QuestionOrder, SurveyResponse, Response
 
-# Create your tests here.
+class SurveyModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testuser")
+        self.survey = Survey.objects.create(id="survey1", user=self.user, title="Test Survey")
+        self.question = Question.objects.create(id="question1", question_text="Sample question?", question_type="text")
+        self.survey_response = SurveyResponse.objects.create(survey=self.survey, id="response1", status="complete")
+        self.response = Response.objects.create(user=self.user, survey_response=self.survey_response, question=self.question, answer="Sample answer")
+
+    def test_survey_creation(self):
+        self.assertEqual(self.survey.user, self.user)
+        self.assertEqual(self.survey.title, "Test Survey")
+
+    def test_relationships(self):
+        self.assertIn(self.survey_response, self.survey.responses.all())
+        self.assertIn(self.response, self.survey_response.responses.all())
+        self.assertEqual(self.response.user, self.user)
+        self.assertEqual(self.response.question, self.question)
