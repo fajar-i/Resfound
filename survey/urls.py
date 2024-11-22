56fad2f79a32418895d57d2db0a010ee4514a752
mSyukri-upi <m.syukri@upi.edu>
2024-11-22 21:13:34 +0700

    clean up

--- survey/urls.py
+++ survey/urls.py
@@ -4,13 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('Insert_Question/<int:survey_id>/', views.Insert_Question, name='Insert_Question'),
-    
-    # Syukri
+    path('edit_survey/<int:survey_id>/', views.create_survey, name='edit_survey'),
     path('list_my_survey/', views.list_my_survey, name='list_my_survey'),
-    path('create_survey/', views.Insert_Question, name='create_survey'),
-    path('edit_survey/<int:survey_id>/', views.create_or_edit_survey, name='edit_survey'),
+    path('create_survey/', views.create_survey, name='create_survey'),
     path('delete_survey/<int:survey_id>/', views.delete_survey, name='delete_survey'),
     path('delete_question/<int:question_id>/', views.delete_question, name='delete_question')
-
 ]
--- survey/views.py
+++ survey/views.py
@@ -9,80 +9,9 @@ from .forms import FormToCreateSurvey, FormToCreateQuestion, FormToCreateChoices
 def home(request):
     return render(request, 'home.html')
 
-# Syukri
 def list_my_survey(request):
     list_semua = Survey.objects.all()
     return render(request, 'my_survey.html', {'surveys': list_semua})
-
-def create_or_edit_survey(request, survey_id=None):
-    if survey_id:
-        survey = Survey.objects.get(pk=survey_id)
-        questions = Question.objects.filter(survey=survey).order_by('question_order')
-        survey_form = FormToCreateSurvey(instance=survey)
-    else:
-        survey = None
-        questions = None
-        survey_form = FormToCreateSurvey()
-
-    is_edit_survey = bool(survey)
-
-    if request.method == 'POST':
-        if 'survey_form' in request.POST:
-            survey_form = FormToCreateSurvey(request.POST, instance=survey)
-            if survey_form.is_valid():
-                survey = survey_form.save(commit=False)
-                survey.user = request.user
-                survey.save()
-                return redirect('list_my_survey')
-            # else:
-                # form tidak valid, error
-        if 'question_form' in request.POST:
-            question_id = request.POST.get('question-id')
-            if question_id:
-                question = Question.objects.get(pk=int(question_id), survey=survey)
-                question_form = FormToCreateQuestion(request.POST, instance=question)
-            else:
-                question_form = FormToCreateQuestion(request.POST)
-            if question_form.is_valid():
-                question = question_form.save(commit=False)
-                question.survey = survey
-                question.save()
-                return redirect('edit_survey', survey_id=survey.id)
-            # else:
-            #     form tidak valid, error
-        else:
-            survey = Survey.objects.get(pk=survey_id)
-            is_edit_survey = bool(survey)
-            question_form = FormToCreateQuestion()
-            if survey:
-                survey_form = FormToCreateSurvey(instance=survey)
-                questions = Question.objects.filter(survey=survey).order_by('question_order')
-            else:
-                survey_form = FormToCreateSurvey()
-                questions = None
-            return render(request, 'create_survey.html', {
-                # list semua "variabel" yang dibutuhkan oleh file html
-                'is_edit_survey': is_edit_survey,
-                'survey_form': survey_form,
-                'question_form': question_form,
-                'questions': questions
-            })
-    
-    is_edit_survey = bool(survey)
-    question_form = FormToCreateQuestion()
-    if survey:
-        survey_form = FormToCreateSurvey(instance=survey)
-        questions = Question.objects.filter(survey=survey).order_by('question_order')
-    else:
-        survey_form = FormToCreateSurvey()
-        questions = None
-    return render(request, 'create_survey.html', {
-        # list semua "variabel" yang dibutuhkan oleh file html
-        'is_edit_survey': is_edit_survey,
-        'survey_form': survey_form,
-        'question_form': question_form,
-        'questions': questions
-    })
 
 def delete_survey(request, survey_id):
     survey = get_object_or_404(Survey, id=survey_id)
@@ -99,12 +28,12 @@ def delete_question(request, question_id):
     if request.method == 'POST' or 'GET':
         survey = question.survey  # Get the related survey
         question.delete()  # Delete the question
-        return redirect('Insert_Question', survey_id=survey.id)
+        return redirect('edit_survey', survey_id=survey.id)
     else:
-        return redirect('Insert_Question', survey_id=question.survey.id)
+        return redirect('edit_survey', survey_id=question.survey.id)
 
 
-def Insert_Question(request, survey_id=None):
+def create_survey(request, survey_id=None):
     # Retrieve existing survey or initialize for creation
     # print(request.POST)
 
@@ -151,7 +80,7 @@ def Insert_Question(request, survey_id=None):
                         choice.question = question
                         choice.save()
 
-            return redirect('Insert_Question', survey_id=survey.id)
+            return redirect('edit_survey', survey_id=survey.id)
 
     # Initialize choice formsets for rendering
     choice_formsets = {}
