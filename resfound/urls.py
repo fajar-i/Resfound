eb6c183341436fb475cf645bcbd8743c4d95a7a0
Bjari <fajariarkan@gmail.com>
2024-12-08 15:51:58 +0700

    1

--- resfound/urls.py
+++ resfound/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect # Hanya perlu diimpor sekali
+from django.shortcuts import redirect  # Hanya perlu diimpor sekali
+from rest_framework import routers
+from survey.viewsets import SurveyViewSets  # Import your viewsets
+
+router = routers.SimpleRouter()
+router.register(r'survey', SurveyViewSets, basename='survey')  # Register the viewset
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('login')),  # Mengarahkan root URL ke /login/
     path('survey/', include('survey.urls')),  # Pastikan survey.urls sudah benar
+    path('api/', include((router.urls, 'core_api'), namespace='core_api')),
 ]
---
+++ survey/viewsets.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+
+from .models import Survey
+from .serializers import SurveySerializer
+
+class SurveyViewSets(viewsets.ModelViewSet):
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
+    permission_classes = [AllowAny]
