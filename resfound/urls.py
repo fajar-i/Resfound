5b953b9c1f25b5f3f72e7fe13d84ffb8d78018d8
Bjari <fajariarkan@gmail.com>
2024-12-16 19:48:30 +0700

    serializer

--- resfound/urls.py
+++ resfound/urls.py
@@ -4,20 +4,21 @@ from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
-from survey.viewsets import SurveyViewSets, PublishViewSets, ProfileViewSets
+from survey.viewsets import ProfileViewSets, SurveyViewSets, PublishViewSets, ChoiceViewSets, QuestionViewSets
 
 router = routers.SimpleRouter()
-router.register(r'survey', SurveyViewSets, basename='survey')
-router.register(r'publish', PublishViewSets, basename="publish")
 router.register(r'profile', ProfileViewSets, basename="profile")
+router.register(r'survey', SurveyViewSets, basename='survey')
+router.register(r'question', QuestionViewSets, basename='question')
+router.register(r'choice', ChoiceViewSets, basename='choice')
+router.register(r'publish', PublishViewSets, basename="publish")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('login/', lambda request: redirect('login')),  # Mengarahkan root URL ke /login/
     path('', include('survey.urls')),
     path('api/', include((router.urls, 'core_api'), namespace='core_api')),
+    path('admin/', admin.site.urls),
+    # path('login/', lambda request: redirect('login')),  # Mengarahkan root URL ke /login/
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- survey/viewsets.py
+++ survey/viewsets.py
@@ -3,10 +3,16 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-from .models import Survey, SurveyResponse, UserProfile, RecommendedSurvey, Response as ResponseModel
-from .serializers import SurveySerializer, PublishSerializer, SurveyResponseSerializer, ResponseSerializer, ProfileSerializer
+from .models import UserProfile, Survey, RecommendedSurvey, Question, ResponseChoice
+from .serializers import SurveySerializer, PublishSerializer, ProfileSerializer, ResponseChoiceSerializer, QuestionSerializer
 
+class ProfileViewSets(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [AllowAny]
+    
 class SurveyViewSets(viewsets.ModelViewSet):
     queryset = Survey.objects.all()
     serializer_class = SurveySerializer
@@ -17,42 +23,13 @@ class PublishViewSets(viewsets.ModelViewSet):
     serializer_class = PublishSerializer
     permission_classes = [AllowAny]
 
-class ProfileViewSets(viewsets.ModelViewSet):
-    queryset = UserProfile.objects.all()
-    serializer_class = ProfileSerializer
+class ChoiceViewSets(viewsets.ModelViewSet):
+    queryset = ResponseChoice.objects.all()
+    serializer_class = ResponseChoiceSerializer
     permission_classes = [AllowAny]
 
+class QuestionViewSets(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = [AllowAny]
 
-
-class SurveyResponsesAPIView(APIView):
-    def get(self, request, survey_id):
-        # Fetch the survey or return a 404 if not found
-        survey = get_object_or_404(Survey, id=survey_id)
-        
-        # Prefetch related data for efficiency
-        survey_responses = SurveyResponse.objects.filter(survey=survey).prefetch_related(
-            'responses__question'
-        )
-
-        # Prepare the response data
-        data = []
-        for response in survey_responses:
-            answers = [
-                {
-                    'question_text': answer.question.question_text,
-                    'answer': answer.answer
-                }
-                for answer in response.responses.all()
-            ]
-            data.append({
-                'respondent_id': response.id,
-                'status': response.status,
-                'last_updated': response.last_updated,
-                'answers': answers,
-            })
-
-        # Return survey details along with all responses
-        return Response({
-            'survey': SurveySerializer(survey).data,
-            'responses': data,
-        })
